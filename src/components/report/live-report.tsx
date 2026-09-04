5da19998ebea35b69c925d46e6f642f35b8f141c
Studio Vitamin D <studiovitamind@Studios-MacBook-Air.local>
2026-09-04 15:28:30 +0530

    Center landscape report flipbook

--- src/components/report/live-report.tsx
+++ src/components/report/live-report.tsx
@@ -248,7 +248,7 @@ export function LiveReport({
         data-current-page={page + 1}
         data-total-pages={pages.length}
       >
-        <div className="report-page-flipbook mx-auto mt-5" style={{ transform: `scale(${zoom})`, transformOrigin: "top center" }}>
+        <div className="report-page-flipbook mx-auto mt-5" style={{ width: "1588px", height: "1123px", transform: `scale(${zoom})`, transformOrigin: "top center" }}>
           <HTMLFlipBook
             ref={flipBookRef}
             className="report-flipbook-canvas"
