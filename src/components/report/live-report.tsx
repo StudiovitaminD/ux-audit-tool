07adf2e8d79459fac0858af8ba970f4eb70853ce
Studio Vitamin D <studiovitamind@Studios-MacBook-Air.local>
2026-09-07 12:53:06 +0530

    Revert "Use consistent page turning mode"
    This reverts commit fa1dfc68ff85c52e8f0b76254221c0ec1fcfb087.

--- src/components/report/live-report.tsx
+++ src/components/report/live-report.tsx
@@ -251,13 +251,14 @@ export function LiveReport({
         <div
           className="report-page-flipbook mx-auto mt-5"
           style={{
-            width: "1588px",
+            width: page === 0 ? "794px" : "1588px",
             height: "1123px",
             transform: `scale(${zoom})`,
             transformOrigin: "top center",
           }}
         >
           <HTMLFlipBook
+            key={page === 0 ? "cover-mode" : "spread-mode"}
             ref={flipBookRef}
             className="report-flipbook-canvas"
             style={{}}
@@ -272,7 +273,7 @@ export function LiveReport({
             startZIndex={0}
             autoSize
             showCover
-            usePortrait={false}
+            usePortrait={page === 0}
             drawShadow
             maxShadowOpacity={0.45}
             flippingTime={1400}
